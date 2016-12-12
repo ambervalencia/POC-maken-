a7bcd3076f615c1b07f5c34d6409579bd7297137
ambervalencia <amber.valencia@hotmail.com>
2016-12-12 20:25:48 +0100

    Update jsprototypemaken.js

--- jsprototypemaken.js
+++ jsprototypemaken.js
@@ -1,3 +1,41 @@
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+var d = new Date();
+var h = addZero(d.getHours());
+var m = addZero(d.getMinutes());
+var s = addZero(d.getSeconds());
+var tijd = h + ":" + m + ":" + s;
+
+var random = Math.random();
+
+var mijnZinnenAvond = new Array();
+mijnZinnenAvond[0] = "Wat heb je vandaag gedaan?";
+mijnZinnenAvond[1] = "Wat zou je graag willen doen?";
+mijnZinnenAvond[2] = "Wat heb je vandaag bereikt?";
+mijnZinnenAvond[3] = "Heb je vandaag iets leuks gedaan?";
+
+var lengteAvond = mijnZinnenAvond.length;
+
+var mijnZinnenOchtend= new Array();
+mijnZinnenOchtend[0] = "Wat ga je doen vandaag?";
+mijnZinnenOchtend[1] = "Wat zou je graag willen doen?";
+mijnZinnenOchtend[2] = "Wat wil je vandaag bereiken?";
+mijnZinnenOchtend[3] = "Hoe wil je je voelen vandaag?";
+
+var lengteOchtend = mijnZinnenOchtend.length;
+
+if (d.getHours() >= 12 && d.getMinutes() >= 00) {
+   document.getElementById("tekst").innerHTML = mijnZinnenAvond[Math.floor(random*(lengteAvond-1))];
+} else{
+  document.getElementById("tekst").innerHTML = mijnZinnenOchtend[Math.floor(random*(lengteOchtend-1))];
+}
+
+
 /*menu code*/
 var menuIcon = document.getElementById("menu-icon");
 var menuList = document.getElementById("menu-list");
@@ -10,26 +48,9 @@ menuIcon.addEventListener('click', function (e) {
 });
 
 
+/* code doet het zinnen veranderen, bij onload*/
 
-/*var colors = ["Wat heb je vandaag gedaan?", "Wat zou je graag willen doen?", "Wat heb je vandaag bereikt?", "Heb je vandaag iets leuks gedaan?"];
-
-window.onload = function(){
-    var arr = document.querySelectorAll("h3");
-    for(var i=0;i<arr.length;i++){
-         arr[i].style.background = bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
-    }
-}
-
-window.onload =function (){
-    var tekst = document.querySelector("h3");
-    for(var i=0;1<tekst.length;i++){
-        tekst[i].
-    }
-}*/
-
-//var button = document.getElementById("button");
-
-var random = Math.random();
+/*var random = Math.random();
 
 var mijnZinnen = new Array();
 mijnZinnen[0] = "Wat heb je vandaag gedaan?";
@@ -41,26 +62,7 @@ var lengte = mijnZinnen.length;
 
 window.onload = function(){
     document.getElementById("tekst").innerHTML = mijnZinnen[Math.floor(random*(lengte-1))];
-}
-
-
-
-
-
- 
-/*var j = 0
-var p = mijnZinnen.length;
-var preBuffer = new Array()
-
-window.onload =function (){
-for (i = 0; i < p; i++){
-   preBuffer[i] = new Array()
-   preBuffer[i].src = mijnZinnen[i]
-}
-}
-
-var whichText = Math.round(Math.random()*(p-1));
-function showText(){
-//This a wrapper for the text array it can be changed if need
-document.write('<h3>'+theText[whichText]+'</h3>');
 }*/
+
+
+
